296ca699ecff5bb7b149476d8370ed76fb31bc55
moeabbas6 <yvu4eZ@protonmail.ch>
2024-09-26 10:08:31 -0400

    incremental mat correction

--- models/marts/finance/fct_payment_methods.sql
+++ models/marts/finance/fct_payment_methods.sql
@@ -14,40 +14,35 @@
 WITH
   int_payments AS (
     SELECT *
-      FROM {{ ref('int_payments') }})
+      FROM {{ ref('int_payments') }}
+      {%- if is_incremental() %}
+      WHERE created_at >= (SELECT DATE_SUB(MAX(order_date), INTERVAL 3 DAY) FROM {{ this }})
+      {%- endif -%})
 
 
   ,fct_payment_methods AS (
     SELECT country_id
-           ,country
-           ,order_id
-           ,COUNT(order_payment_id) AS nb_payments
-           ,IF(COUNT(DISTINCT payment_method) > 1, 'multiple', 'single') AS payment_methods
-           ,MAX(created_at) AS order_date
-           ,CASE 
-            WHEN SUM(IF(payment_status = 'successful', 1, 0)) <> COUNT(order_payment_id) THEN 'failed'
-            ELSE 'successful' END AS order_status
-           ,SUM(gross_revenue) AS total_gross_revenue
-           ,SUM(payment_fee) AS total_payment_fees
-           {% for payment_method in payment_methods -%}
-           ,SUM(CASE WHEN payment_method = '{{ payment_method }}' THEN payment_fee ELSE 0 END) AS {{ payment_method }}_payment_fees
-           {% endfor -%}
-           {% for payment_method in payment_methods -%}
-           ,SUM(CASE WHEN payment_method = '{{ payment_method }}' THEN gross_revenue ELSE 0 END) AS {{ payment_method }}_amount
-           {% endfor -%}
+          ,country
+          ,order_id
+          ,COUNT(order_payment_id) AS nb_payments
+          ,IF(COUNT(DISTINCT payment_method) > 1, 'multiple', 'single') AS payment_methods
+          ,MAX(created_at) AS order_date
+          ,CASE 
+           WHEN SUM(IF(payment_status = 'successful', 1, 0)) <> COUNT(order_payment_id) THEN 'failed'
+           ELSE 'successful' END AS order_status
+          ,SUM(gross_revenue) AS total_gross_revenue
+          ,SUM(payment_fee) AS total_payment_fees
+          {% for payment_method in payment_methods -%}
+          ,SUM(CASE WHEN payment_method = '{{ payment_method }}' THEN payment_fee ELSE 0 END) AS {{ payment_method }}_payment_fees
+          {% endfor -%}
+          {% for payment_method in payment_methods -%}
+          ,SUM(CASE WHEN payment_method = '{{ payment_method }}' THEN gross_revenue ELSE 0 END) AS {{ payment_method }}_amount
+          {% endfor -%}
       FROM int_payments
       GROUP BY country_id
               ,country
               ,order_id)
 
 
-  ,final AS (
   SELECT *
     FROM fct_payment_methods
-      {%- if is_incremental() %}
-      WHERE order_date >= (SELECT DATE_SUB(MAX(order_date), INTERVAL 3 DAY) FROM {{ this }})
-      {%- endif -%})
-
-
-  SELECT *
-    FROM final
