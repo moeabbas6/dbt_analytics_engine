49b1581a5b476bc67762269f5332d8ad67c6078f
moeabbas6 <yvu4eZ@protonmail.ch>
2024-09-28 08:40:06 -0400

    dim customers tests

--- models/marts/customers/dim_customers.sql
+++ models/marts/customers/dim_customers.sql
@@ -2,10 +2,14 @@
     materialized = "incremental",
     unique_key = "customer_id",
     on_schema_change = 'append_new_columns',
-    cluster_by = 'last_order'
+    partition_by = {
+      "field": "segment_id",
+      "data_type": "int64",
+      "range": { "start": 0, "end": 100, "interval": 1}},
+    cluster_by = 'last_order_date'
 )}}
 
-{% set count_fields = ['shipping_id', 'return_id'] %}
+{% set count_fields = ['order_id', 'shipping_id', 'return_id'] %}
 {% set avg_fields = ['fulfillment_days', 'nps_score'] %}
 {% set sum_fields = ['gross_revenue', 'tax_amount', 'net_revenue_before_tax', 'net_revenue_after_tax',
                      'cogs', 'returned_cogs', 'refund_amount', 'payment_fee', 'cm'] %}
@@ -15,7 +19,7 @@ WITH
     SELECT *
       FROM {{ ref('fct_orders') }}
       {%- if is_incremental() %}
-      WHERE DATE(order_date) >= (SELECT DATE_SUB(MAX(last_order), INTERVAL 3 DAY) FROM {{ this }})
+      WHERE DATE(order_date) >= (SELECT DATE_SUB(MAX(last_order_date), INTERVAL 3 DAY) FROM {{ this }})
       {%- endif %})
 
 
@@ -23,10 +27,9 @@ WITH
     SELECT customer_id
           ,first_name
           ,last_name
-          ,COUNT(order_id) AS nb_orders
           ,DATE_DIFF(CURRENT_DATE, MAX(order_date), DAY) AS last_order_days
-          ,MIN(order_date) AS first_order
-          ,MAX(order_date) AS last_order
+          ,MIN(order_date) AS first_order_date
+          ,MAX(order_date) AS last_order_date
           {% for count_field in count_fields -%}
           ,COUNT({{ count_field }}) AS nb_{{ count_field }}
           {% endfor -%}
@@ -51,23 +54,62 @@ WITH
             ELSE '>365 days'
             END AS recency
           ,CASE 
-            WHEN nb_orders > 2  THEN "3+ orders"
-            WHEN nb_orders = 2  THEN "2 orders"
-            WHEN nb_orders = 1  THEN "1 order"
+            WHEN nb_order_id > 2  THEN "3+ orders"
+            WHEN nb_order_id = 2  THEN "2 orders"
+            WHEN nb_order_id = 1  THEN "1 order"
             END AS frequency
           ,CASE 
-            WHEN nb_orders > 2 AND last_order_days <= 90 THEN "Champion"
-            WHEN nb_orders = 2 AND last_order_days <= 90 THEN "Rising Star"
-            WHEN nb_orders = 1 AND last_order_days <= 90 THEN "Active Newbie"
-            WHEN nb_orders > 2 AND last_order_days <= 180 AND last_order_days > 90 THEN "Occasional"
-            WHEN nb_orders = 2 AND last_order_days <= 180 AND last_order_days > 90 THEN "Need Attention"
-            WHEN nb_orders = 1 AND last_order_days <= 180 AND last_order_days > 90 THEN "Newbies About to Sleep / Asleep"
-            WHEN nb_orders > 2 AND last_order_days <= 365 AND last_order_days > 180 THEN "Cannot / Almost Loose Them"
-            WHEN nb_orders = 2 AND last_order_days <= 365 AND last_order_days > 180 THEN "At Risk / Lost 2nd Buyer"
-            WHEN nb_orders = 1 AND last_order_days <= 365 AND last_order_days > 180 THEN "Unhappy / Lost 1st Buyer"
-            ELSE "Hibernating Customers" END AS segment
+            WHEN nb_order_id > 2 AND last_order_days <= 90 THEN "Loyal Leader"
+            WHEN nb_order_id = 2 AND last_order_days <= 90 THEN "Growing Patron"
+            WHEN nb_order_id = 1 AND last_order_days <= 90 THEN "New Enthusiast"
+            WHEN nb_order_id > 2 AND last_order_days <= 180 AND last_order_days > 90 THEN "Casual Shopper"
+            WHEN nb_order_id = 2 AND last_order_days <= 180 AND last_order_days > 90 THEN "Waning Loyalty"
+            WHEN nb_order_id = 1 AND last_order_days <= 180 AND last_order_days > 90 THEN "Slipping Newcomer"
+            WHEN nb_order_id > 2 AND last_order_days <= 365 AND last_order_days > 180 THEN "At-Risk Regular"
+            WHEN nb_order_id = 2 AND last_order_days <= 365 AND last_order_days > 180 THEN "Vanished Buyer"
+            WHEN nb_order_id = 1 AND last_order_days <= 365 AND last_order_days > 180 THEN "One-Time Buyer"
+            ELSE "Dormant Customer" END AS segment
         FROM customers)
 
 
+  ,final AS (
+    SELECT customer_id
+          ,first_name
+          ,last_name
+          ,first_order_date
+          ,last_order_date
+          ,last_order_days
+          ,recency
+          ,nb_order_id
+          ,frequency
+          ,segment
+          ,CASE segment
+            WHEN "Loyal Leader" THEN 1
+            WHEN "Growing Patron" THEN 2
+            WHEN "New Enthusiast" THEN 3
+            WHEN "Casual Shopper" THEN 4
+            WHEN "Waning Loyalty" THEN 5
+            WHEN "Slipping Newcomer" THEN 6
+            WHEN "At-Risk Regular" THEN 7
+            WHEN "Vanished Buyer" THEN 8
+            WHEN "One-Time Buyer" THEN 9
+            WHEN "Dormant Customer" THEN 10
+            END AS segment_id
+          ,nb_shipping_id
+          ,nb_return_id
+          ,avg_fulfillment_days
+          ,avg_nps_score
+          ,total_gross_revenue
+          ,total_tax_amount
+          ,total_net_revenue_before_tax
+          ,total_net_revenue_after_tax
+          ,total_cogs
+          ,total_returned_cogs
+          ,total_refund_amount
+          ,total_payment_fee
+          ,total_cm
+      FROM rfm)
+
+
   SELECT *
-    FROM rfm
+    FROM final
