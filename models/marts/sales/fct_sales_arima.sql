a207cd5b7a47c854d992e86ad3f0f5918e01f2a3
moeabbas6 <yvu4eZ@protonmail.ch>
2024-10-02 22:18:49 -0400

    fct_sales_arima

--- models/marts/sales/fct_sales_arima.sql
+++ models/marts/sales/fct_sales_arima.sql
@@ -4,8 +4,7 @@
         'model_type': 'ARIMA_PLUS',
         'time_series_timestamp_col' : 'date',
         'time_series_data_col' : 'sales',
-        'horizon' : 30,
-        'auto_arima' : TRUE
+        'horizon' : 30
         }
 )}}
 
