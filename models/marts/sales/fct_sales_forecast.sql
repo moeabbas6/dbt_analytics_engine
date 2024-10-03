4011bc7832e0cb8999159e47219c6653cad0270f
moeabbas6 <yvu4eZ@protonmail.ch>
2024-10-03 08:37:21 -0400

    fct_sales_forecast_correction

--- models/marts/sales/fct_sales_forecast.sql
+++ models/marts/sales/fct_sales_forecast.sql
@@ -1,10 +1,19 @@
 
 
-WITH
-  fct_sales_forecast AS (
+WITH actuals AS (
+  SELECT *
+    FROM {{ ref('fct_sales_timeseries') }})
+
+
+  ,forecasts AS (
     SELECT DATE(forecast_timestamp) AS date
           ,forecast_value AS sales_forecast
       FROM ML.FORECAST(MODEL {{ ref('ml_sales_arima') }}, STRUCT(30 AS horizon)))
 
+  ,final AS (
+    SELECT *
+      FROM actuals
+      FULL JOIN forecasts USING (date))
+
   SELECT *
-    FROM fct_sales_forecast
+    FROM final
