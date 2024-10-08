39ab701bcfad12db84a9918a3a82e1a6f615489b
moeabbas6 <yvu4eZ@protonmail.ch>
2024-10-08 13:21:28 -0400

    refactoring fct cohort retention

--- models/marts/cohorts/fct_cohort_retention.sql
+++ models/marts/cohorts/fct_cohort_retention.sql
@@ -11,14 +11,14 @@ WITH
 
 
   ,orders_with_cohort AS (
-    SELECT o.customer_id
-          ,o.order_id
-          ,DATE_TRUNC(o.order_date, MONTH) AS order_month
-          ,c.cohort_month
-          ,DATE_DIFF(DATE_TRUNC(o.order_date, MONTH), c.cohort_month, MONTH) AS elapsed_month
-      FROM {{ ref('fct_orders') }} o
-      JOIN customer_cohorts c ON o.customer_id = c.customer_id
-      WHERE DATE_DIFF(DATE_TRUNC(o.order_date, MONTH), c.cohort_month, MONTH) >= 0)
+    SELECT customer_id
+          ,order_id
+          ,DATE_TRUNC(order_date, MONTH) AS order_month
+          ,cohort_month
+          ,DATE_DIFF(DATE_TRUNC(order_date, MONTH), cohort_month, MONTH) AS elapsed_month
+      FROM {{ ref('fct_orders') }}
+      JOIN customer_cohorts USING (customer_id)
+      WHERE DATE_DIFF(DATE_TRUNC(order_date, MONTH), cohort_month, MONTH) >= 0)
 
   
   ,cohort_activity AS (
@@ -62,10 +62,10 @@ WITH
           ,cem.elapsed_month
           ,COUNT(DISTINCT cla.customer_id) AS customers_retained
           ,cs.cohort_size
-          ,COUNT(DISTINCT cla.customer_id) / cs.cohort_size AS cumulative_retention_rate
+          ,SAFE_DIVIDE(COUNT(DISTINCT cla.customer_id), cs.cohort_size) AS cumulative_retention_rate
       FROM cohort_elapsed_months cem
       LEFT JOIN customer_last_elapsed_month cla ON cem.cohort_month = cla.cohort_month 
-                                              AND cla.last_elapsed_month >= cem.elapsed_month
+                                               AND cla.last_elapsed_month >= cem.elapsed_month
       JOIN cohort_sizes cs ON cem.cohort_month = cs.cohort_month
       GROUP BY cem.cohort_month
               ,cem.elapsed_month
@@ -82,20 +82,20 @@ WITH
 
 
   ,point_in_time_retention AS (
-    SELECT pta.cohort_month
-          ,pta.elapsed_month
-          ,pta.active_customers
-          ,cs.cohort_size
-          ,pta.active_customers / cs.cohort_size AS point_in_time_retention_rate
-      FROM point_in_time_activity pta
-      JOIN cohort_sizes cs ON pta.cohort_month = cs.cohort_month)
+    SELECT cohort_month
+          ,elapsed_month
+          ,active_customers
+          ,cohort_size
+          ,SAFE_DIVIDE(active_customers, cohort_size) AS point_in_time_retention_rate
+      FROM point_in_time_activity
+      JOIN cohort_sizes USING(cohort_month))
 
 
   ,final_join AS (
     SELECT ctr.cohort_month
           ,ctr.elapsed_month
           ,ctr.customers_retained
-          ,(ctr.cohort_size - ctr.customers_retained) AS churned_users
+          ,SAFE_SUBTRACT(ctr.cohort_size, ctr.customers_retained) AS churned_users
           ,ctr.cohort_size
           ,ctr.cumulative_retention_rate
           ,ptr.active_customers
